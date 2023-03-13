2b9fa62fe7b38ef6d0fda982c46cfe4b897ce5c4
Thant Wai Soe <thantwaisoe456@gmail.com>
2023-03-14 01:00:04 +0630

    added watch mode( Vue 3 feature) for user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script>
+import { watch } from 'vue'
 import Navbar from '../components/Navbar'
+import getUser from '@/service/getUser'
+import { useRouter } from 'vue-router'
 export default {
   components: { Navbar },
+  setup(){
+    let {user} = getUser()
+    let router = useRouter()
+    console.log(user.value)
+    watch(user, () => {
+      if(!user.value){
+        router.push({name: 'Welcome'})
+      }
+    })
+  }
 
 }
 </script>
