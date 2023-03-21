c1fbc21630c90046781f8912f322d9c08163f37f
Thant Wai Soe <thantwaisoe456@gmail.com>
2023-03-21 14:36:23 +0630

    added chat window desingn and setup done for css for chat window

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="chat-window">
+    <div class="messages">
+        <div class="single">
+            <span class="created-at"> 3 min ago</span>
+            <span class="name">OO</span>
+            <span class="message">Hello Test Messages</span>
+        </div>
+        <div class="single">
+            <span class="created-at"> 3 min ago</span>
+            <span class="name">OO</span>
+            <span class="message">Hello Test Messages</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.chat-window {
+        background: #fafafa;
+        padding: 30px 20px;
+      }
+      .single {
+        margin: 12px 0;
+      }
+      .created-at {
+        display: block;
+        color: #999;
+        font-size: 12px;
+        margin-bottom: 4px;
+      }
+      .name {
+        font-weight: bold;
+        margin-right: 6px;
+      }
+      .messages {
+        max-height: 400px;
+        overflow: auto;
+      }
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow />
     <ChatForm />
   </div>
 </template>
 
 <script>
+import ChatWindow from '../components/ChatWindow'
 import { watch } from 'vue'
 import Navbar from '../components/Navbar'
 import ChatForm from '../components/ChatForm'
 import getUser from '@/service/getUser'
 import { useRouter } from 'vue-router'
 export default {
-  components: { Navbar, ChatForm },
+  components: {
+    ChatWindow, Navbar, ChatForm },
   setup(){
     let {user} = getUser()
     let router = useRouter()
@@ -27,6 +30,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
