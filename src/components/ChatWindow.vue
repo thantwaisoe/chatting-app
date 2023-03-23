f38ef67c8944627a0fc6a04259abbc168239760a
Thant Wai Soe <thantwaisoe456@gmail.com>
2023-03-23 15:06:38 +0630

    fixed date format

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="chat-window">
-    <div class="messages" v-for="message in allMessaegs" :key="message.id">
-        <div class="single">
-            <span class="created-at"> {{message.created_at ?  message.created_at.toDate() : 'fetching'}}</span>
-            <span class="name">{{message.senderName}}</span>
-            <span class="message">{{ message.message }}</span>
-        </div>
+    <div class="messages">
+      <div class="single" v-for="message in formattedDate" :key="message.id">
+        <span class="created-at"> {{ message.created_at ? message.created_at : ' ' }}</span>
+        <span class="name">{{ message.senderName }}</span>
+        <span class="message">{{ message.message }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -13,49 +13,59 @@
 <script>
 import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
 import { db } from '@/firebase/config';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { formatDistanceToNow } from 'date-fns'
 export default {
-    setup(){
-        let allMessaegs = ref([])
-       const dbRef =  collection(db, 'messages')
-       const q = query(dbRef, orderBy('created_at'))
-       onSnapshot(q, data => {
-        let result = []
-          data.docs.forEach(eachRow => {
-            let document = {...eachRow.data(), id: eachRow.id}
-            result.push(document)
-          })
-          allMessaegs.value = result
-       })
+  setup() {
+    let allMessaegs = ref([])
+    let formattedDate = computed(() => {
+      return allMessaegs.value.map((msg) => {
+        return { ...msg, created_at: formatDistanceToNow(msg.created_at.toDate(), { includeSeconds: true, addSuffix: true }) }
+      })
+    })
+    const dbRef = collection(db, 'messages')
+    const q = query(dbRef, orderBy('created_at'))
+    onSnapshot(q, data => {
+      let result = []
+      data.docs.forEach(eachRow => {
+        let document = { ...eachRow.data(), id: eachRow.id }
+        result.push(document)
+      })
+      allMessaegs.value = result
+    })
 
-       return {
-        allMessaegs
-       }
+    return {
+      allMessaegs,
+      formattedDate
     }
+  }
 }
 </script>
 
 <style scoped>
 .chat-window {
-        background: #fafafa;
-        padding: 30px 20px;
-      }
-      .single {
-        margin: 12px 0;
-      }
-      .created-at {
-        display: block;
-        color: #999;
-        font-size: 12px;
-        margin-bottom: 4px;
-      }
-      .name {
-        font-weight: bold;
-        font-size: 20px;
-        margin-right: 6px;
-      }
-      .messages {
-        max-height: 400px;
-        overflow: auto;
-      }
-</style>
+  background: #fafafa;
+  padding: 30px 20px;
+}
+
+.single {
+  margin: 12px 0;
+}
+
+.created-at {
+  display: block;
+  color: #999;
+  font-size: 12px;
+  margin-bottom: 4px;
+}
+
+.name {
+  font-weight: bold;
+  font-size: 20px;
+  margin-right: 6px;
+}
+
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}</style>
